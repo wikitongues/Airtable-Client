e94ce09f0216e83f92617b0467dfc2a21e6090e5
Scott Rohrer <smrohrer@alumni.cmu.edu>
2023-09-24 17:50:09 -0400

    Add cellFormat parameter

--- wt_airtable_client/airtable_http_client.py
+++ wt_airtable_client/airtable_http_client.py
@@ -1,12 +1,18 @@
 import urllib.parse
 from abc import ABC, abstractmethod
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 
 import requests
 from requests import Response
 
 from .airtable_connection_info import AirtableConnectionInfo
 from .airtable_table_info import AirtableTableInfo
+
+
+class CellFormat(Enum):
+    JSON = "json"
+    STRING = "string"
 
 
 class IAirtableHttpClient(ABC):
@@ -19,7 +25,13 @@ class IAirtableHttpClient(ABC):
 
     @abstractmethod
     def list_records(
-        self, page_size: Optional[int] = 100, offset: Optional[str] = None, max_records: Optional[int] = None
+        self,
+        page_size: Optional[int] = 100,
+        offset: Optional[str] = None,
+        max_records: Optional[int] = None,
+        cell_format: Optional[CellFormat] = None,
+        time_zone: Optional[str] = None,
+        user_locale: Optional[str] = None,
     ) -> Response:
         """
         List records
@@ -87,21 +99,32 @@ class AirtableHttpClient(IAirtableHttpClient):
 
         self._id_column = table_info.id_column
 
+    @staticmethod
+    def _handle_cell_format_params(
+        params: List[str],
+        cell_format: Optional[CellFormat] = None,
+        time_zone: Optional[str] = None,
+        user_locale: Optional[str] = None,
+    ) -> None:
+        assert (
+            cell_format != CellFormat.STRING or time_zone and user_locale
+        ), "time_zone and user_locale are required if cell_format is string"
+
+        if cell_format == CellFormat.STRING:
+            params.append(f"cellFormat={cell_format.value}")
+            params.append(f"timeZone={time_zone}")
+            params.append(f"userLocale={user_locale}")
+
     def list_records(
-        self, page_size: Optional[int] = 100, offset: Optional[str] = None, max_records: Optional[int] = None
+        self,
+        *,
+        page_size: Optional[int] = 100,
+        offset: Optional[str] = None,
+        max_records: Optional[int] = None,
+        cell_format: Optional[CellFormat] = None,
+        time_zone: Optional[str] = None,
+        user_locale: Optional[str] = None,
     ) -> Response:
-        """
-        List records
-
-        Args:
-            page_size (int, optional): Page size. Defaults to 100.
-            offset (str, optional): Offset for pagination. Defaults to None.
-            max_records (int, optional): Max records. Defaults to None.
-
-        Returns:
-            Response: Response from Airtable API
-        """
-
         params = [f"maxRecords={max_records}"]
 
         if page_size is not None:
@@ -110,54 +133,48 @@ class AirtableHttpClient(IAirtableHttpClient):
         if offset is not None:
             params.append(f"offset={offset}")
 
+        AirtableHttpClient._handle_cell_format_params(params, cell_format, time_zone, user_locale)
+
         url = f'{self._route}?{"&".join(params)}'
 
         return requests.get(url, headers=self._headers)
 
-    def get_record(self, id: str) -> Response:
-        """
-        Get record
-
-        Args:
-            id (str): Id of record
-
-        Returns:
-            Response: Response from Airtable API
-        """
-
+    def get_record(
+        self,
+        id: str,
+        *,
+        cell_format: Optional[CellFormat] = None,
+        time_zone: Optional[str] = None,
+        user_locale: Optional[str] = None,
+    ) -> Response:
         formula = urllib.parse.quote_plus(f"FIND('{id}', {{{self._id_column}}}) != 0")
-        url = f"{self._route}?filterByFormula={formula}"
+        params = [f"filterByFormula={formula}"]
+        AirtableHttpClient._handle_cell_format_params(params, cell_format, time_zone, user_locale)
+
+        url = f'{self._route}?{"&".join(params)}'
 
         return requests.get(url, headers=self._headers)
 
-    def get_records_by_fields(self, fields: dict) -> Response:
-        """
-        Get any records matching the given fields
-
-        Args:
-            fields (dict): Dictionary of fields
-
-        Returns:
-            Response: Response from Airtable API
-        """
+    def get_records_by_fields(
+        self,
+        fields: dict,
+        *,
+        cell_format: Optional[CellFormat] = None,
+        time_zone: Optional[str] = None,
+        user_locale: Optional[str] = None,
+    ) -> Response:
         formula = "AND("
         formula += ",".join(["{" + key + "}='" + fields[key] + "'" for key in sorted(fields) if fields[key]])
         formula += ")"
         formula = urllib.parse.quote_plus(formula)
-        url = f"{self._route}?filterByFormula={formula}"
+        params = [f"filterByFormula={formula}"]
+        AirtableHttpClient._handle_cell_format_params(params, cell_format, time_zone, user_locale)
+
+        url = f'{self._route}?{"&".join(params)}'
 
         return requests.get(url, headers=self._headers)
 
     def create_record(self, fields: dict) -> Response:
-        """Create record
-
-        Args:
-            fields (dict): Dictionary of fields
-
-        Returns:
-            Response: Response from Airtable API
-        """
-
         json_obj = {"records": [{"fields": fields}]}
 
         headers = {**self._headers, "Content-Type": "application/json"}
