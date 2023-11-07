3c6713cf96c1da86be19ccaa0ea72a35faf7c670
Scott Rohrer <smrohrer@alumni.cmu.edu>
2023-11-07 14:16:57 -0500

    Remove pagination params from table info class

--- wt_airtable_client/airtable_table_info.py
+++ wt_airtable_client/airtable_table_info.py
@@ -1,6 +1,3 @@
-from typing import Optional
-
-
 class AirtableTableInfo:
     """
     Information for retrieving data from a Table on Airtable
@@ -10,9 +7,6 @@ class AirtableTableInfo:
         self,
         name: str,
         id_column: str,
-        offset: Optional[str] = None,
-        page_size: Optional[int] = 100,
-        max_records: Optional[int] = 100,
     ) -> None:
         """
         Construct AirtableTableInfo
@@ -20,13 +14,7 @@ class AirtableTableInfo:
         Args:
             name (str): Table name
             id_column (str): Name of identifier column
-            offset (str): Offset value for pagination
-            page_size (int, optional): Number of records returned per request
-            max_records (int, optional): Max records to retrieve
         """
 
         self.name = name
         self.id_column = id_column
-        self.page_size = page_size
-        self.offset = offset
-        self.max_records = max_records
